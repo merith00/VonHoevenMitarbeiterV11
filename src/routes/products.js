0e14b947020292e6e66d0d43e261db3c8fe219f0
Merith00 <merithholtmann00@gmail.com>
2024-02-16 08:24:52 +0100

    KMLzusätzlicherschlag

--- src/routes/products.js
+++ src/routes/products.js
@@ -16,11 +16,14 @@ const getUpdateDatenVomKunden = require('../database/oracle').getUpdateDatenVomK
 const kundenzumloeschen = require('../database/oracle').kundenzumloeschen
 const changeNeunzigCm = require('../database/oracle').changeNeunzigCm
 const getProductIDs = require('../database/oracle').getProductIDs
+const getDocumentInformation = require('../database/oracle').getDocumentInformation
 
 
 
 var path = require('path');
 const tokml = require('tokml');
+
+//const { PDFDocument, rgb } = require('pdf-lib');
 
 const getInformationsForGenerateKmlFile = require('../database/oracle').getInformationsForGenerateKmlFile
 const funktionFleacheSollBearbeitetWerden = require('../database/oracle').funktionFleacheSollBearbeitetWerden
@@ -34,8 +37,9 @@ const fileUpload = require('express-fileupload');
 const JSZip = require('jszip');
 const proj4 = require('proj4');
 const { createCanvas, loadImage } = require('canvas');
-const fs = require('fs');
+const fs = require('fs').promises;
 const multer = require('multer');
+const { log } = require('console');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
@@ -126,8 +130,7 @@ function readDataFromKMLFile(file, res,req, selectedOption, register) {
   const kmlData = file.data.toString('utf8');
   const xmlDoc = new DOMParser().parseFromString(kmlData);
   const geoJSON = togeojson.kml(xmlDoc);
-
-  
+ 
 
   onHightLight(geoJSON, res,req,selectedOption, register)
 }
@@ -320,6 +323,111 @@ router.put('/setUpdateDatenVomKunden', async (req,res) => {
     }
   } 
 });
+
+
+const PDFDocument = require('pdfkit');
+
+// Funktion zum Erstellen der PDF-Datei
+function createPDFDocument() {
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream('output.pdf')); // PDF-Dokument erstellen
+  
+  // Füge den Inhalt zur PDF-Datei hinzu
+  doc.text('Hallo, ich bin eine PDF-Datei.');
+  
+  doc.end(); // PDF-Datei abschließen
+}
+
+
+
+router.put('/createLUFADocument', async (req, res) => {
+  if (req.isAuthenticated()) {
+    const productIDs = req.body.productIDs;
+    try {
+      /*const documentInformation = await getDocumentInformation(productIDs, res);
+      const mergedPdfBytes = await fillPDF(documentInformation);
+
+      // Setzen der Response-Header für das PDF
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename="gefuelltesFormular_all.pdf"');
+
+      // Senden des PDF an den Benutzer
+      res.send(mergedPdfBytes);*/
+
+      createPDFDocument();
+
+      res.download('output.pdf', 'Fleachen' + kundennummer + '.pdf'); // Datei herunterladen
+
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
+
+async function fillPDF(documentInformation) {
+  try {
+    const mergedPdfDoc = await PDFDocument.create();
+    //const pdfPath = 'C:/Users/merit/Downloads/Untersuchungsauftrag N min.pdf';
+    const pdfPath = 'C:/Users/merit/Downloads/BeispielPDF.pdf';
+    const pdfBytes = await fs.readFile(pdfPath);
+
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const form = pdfDoc.getForm();
+
+    const read = fs.readFile(pdfPath);
+    //const pdfFilePath = 'meine_pdf_datei.pdf';
+
+    // PDF-Datei in einen Buffer einlesen
+    fs.readFile(pdfPath, (err, data) => {
+      if (err) {
+        console.error('Fehler beim Lesen der PDF-Datei:', err);
+        return;
+      }
+
+      
+      // 'data' ist jetzt ein Buffer-Objekt, das den Inhalt der PDF-Datei enthält
+      console.log('PDF-Buffer:', data);
+    });
+
+    /*for (let i = 0; i < documentInformation.fleachenInformationen.length; i++) {
+      form.getFields()[9].setText(documentInformation.kundenInformation.NACHNAME +', ' + documentInformation.kundenInformation.VORNAME);
+      form.getFields()[8].setText(documentInformation.kundenInformation.STRASSE +', ' + documentInformation.kundenInformation.HAUSNUMMER);
+      form.getFields()[7].setText(documentInformation.kundenInformation.POSTLEITZAHL +', ' + documentInformation.kundenInformation.ORT);
+      form.getFields()[6].setText(''+(documentInformation.kundenInformation.TELEFONNUMMER)+'');
+      form.getFields()[5].setText(documentInformation.kundenInformation.E_MAIL);
+      form.getFields()[11].setText(''+documentInformation.kundenInformation.KUNDENNUMMER+'');
+      form.getFields()[31].setText(documentInformation.fleachenInformationen[i][1]+', '+ documentInformation.fleachenInformationen[i][0]);
+
+      await pdfDoc.save();
+      const pages = await mergedPdfDoc.copyPages(pdfDoc, [0]);
+      pages.forEach((page) => {
+          mergedPdfDoc.addPage(page);
+      });
+    }*/
+
+    var mergedPdfBytes = await mergedPdfDoc.save();
+    mergedPdfBytes = pdfToBlob(mergedPdfBytes)
+    return pdfPath;
+  } catch (error) {
+    console.log('Error: ' + error);
+    throw error;
+  }
+}
+
+async function pdfToBlob(mergedPdfBytes) {
+  // PDF-Datei als ArrayBuffer einlesen
+  //const pdfArrayBuffer = await fetch(pdfFilePath).then(response => response.arrayBuffer());
+
+  // ArrayBuffer in einen Blob umwandeln
+  const pdfBlob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
+  console.log(pdfBlob)
+
+  return pdfBlob;
+}
 
 
 router.put('/changeNeunzigCm', async (req, res) => {
@@ -526,13 +634,18 @@ async function onHightLight(data, res,req,selectedOption,register , anzahldateie
 
     data.features.forEach(feature => {
 
+          
+      var SCHLAG_NR, SCHLAG_NAME
 
-    
+      try{
+        SCHLAG_NAME = getDataFromDiscription(feature.properties.description.value, 'SCHLAG=' , '<BR>' , '-')
+        SCHLAG_NR = getDataFromDiscription(feature.properties.description.value, 'SCHLAG=' , '-', null)
+      }catch(error){}
 
 
-      var schlagBez = getFirstNonNull(feature.properties.SCHLAGBEZ, feature.properties.FL_NAME, feature.properties.name, feature.properties.SCHLAG_NAM);
-      //schlagBez = Buffer.from(schlagBez, 'latin1').toString('utf-8');
-      var FleachenID = getFirstNonNull(feature.properties.SCHLAGNR, feature.properties.SCHLAG_NR, feature.properties.FL_ID);
+      var schlagBez = getFirstNonNull(feature.properties.name, feature.properties.SCHLNAME, feature.properties.name, feature.properties.SCHLAGBEZ, feature.properties.FL_NAME, feature.properties.SCHLAG_NAM,SCHLAG_NAME);
+      var FleachenID = getFirstNonNull(SCHLAG_NR, feature.properties.SCHLNR+''+feature.properties.PRNR_ZAHL,feature.properties.BU_Schlnr,feature.properties.SCHLAGNR, feature.properties.SCHLAG_NR, feature.properties.FL_ID);
+
       var dateValue = feature.properties.BEPROBENAB;
       var NUTZUNG = feature.properties.NUTZUNG
 
@@ -616,7 +729,6 @@ async function onHightLight(data, res,req,selectedOption,register , anzahldateie
   } else {
     var statuscodeEinKunde = 100;
 
-
     if(register){
       try {
         var KUNDEN_NR = data.features[0].properties.KUNDEN_NR;
@@ -624,6 +736,10 @@ async function onHightLight(data, res,req,selectedOption,register , anzahldateie
           KUNDEN_NR = data.features[0].properties.AUFTRAGGEB;
         }
 
+        try{
+          KUNDEN_NR = getDataFromDiscription(data.features[0].properties.description.value, 'INFO=', '_',null)
+        } catch (error){}
+      
         if(KUNDEN_NR === undefined && selectedOption > 1){
           KUNDEN_NR = req.body.kundenid
         }      
@@ -641,9 +757,6 @@ async function onHightLight(data, res,req,selectedOption,register , anzahldateie
         res.sendStatus(400)   
        }
     }
-
-    
-
     if(statuscodeEinKunde===123){
       handleResponse(statuscodeEinKunde, res, 0);
     } else {
@@ -655,15 +768,18 @@ async function onHightLight(data, res,req,selectedOption,register , anzahldateie
 
         data.features.forEach(feature => {
 
-
-
-
-
-          var schlagBez = getFirstNonNull(feature.properties.name, feature.properties.SCHLNAME, feature.properties.name, feature.properties.SCHLAGBEZ, feature.properties.FL_NAME, feature.properties.SCHLAG_NAM);
-
-          //schlagBez = Buffer.from(schlagBez, 'latin1').toString('utf-8');
           
-          var FleachenID = getFirstNonNull(feature.properties.BU_Schlnr,feature.properties.SCHLNR+''+feature.properties.PRNR_ZAHL,feature.properties.SCHLAGNR, feature.properties.SCHLAG_NR, feature.properties.FL_ID);
+          var SCHLAG_NR = null, SCHLAG_NAME = null
+
+          try{
+            SCHLAG_NAME = getDataFromDiscription(feature.properties.description.value, 'SCHLAG=' , '<BR>' , '-')
+            SCHLAG_NR = getDataFromDiscription(feature.properties.description.value, 'SCHLAG=' , '-', null)
+          }catch(error){}
+
+
+          var schlagBez = getFirstNonNull(feature.properties.name, feature.properties.SCHLNAME, feature.properties.name, feature.properties.SCHLAGBEZ, feature.properties.FL_NAME, feature.properties.SCHLAG_NAM,SCHLAG_NAME);
+
+          var FleachenID = getFirstNonNull(SCHLAG_NR, feature.properties.SCHLNR+''+feature.properties.PRNR_ZAHL,feature.properties.BU_Schlnr,feature.properties.SCHLAGNR, feature.properties.SCHLAG_NR, feature.properties.FL_ID);
           var dateValue = feature.properties.BEPROBENAB;
 
 
@@ -742,9 +858,31 @@ async function onHightLight(data, res,req,selectedOption,register , anzahldateie
   }
 }
 
+
+function getDataFromDiscription(discriptionData,firstCut,lastCut,middelCut){
+  var inputString = discriptionData
+  var startIndex = inputString.indexOf(firstCut);
+  if (startIndex !== -1) {
+      startIndex += firstCut.length;
+      var endIndex = inputString.indexOf(lastCut, startIndex);
+      if (endIndex !== -1) {
+        if(middelCut===null){
+          return inputString.substring(startIndex, endIndex);
+        } else {
+          return inputString.substring(startIndex, endIndex).split(middelCut)[1];
+        }
+      }
+
+    }
+    return undefined;
+}
+
 function getFirstNonNull(...values) {
-  for (const value of values) {
+  for (var value of values) {
     if (value !== undefined && value !== null) {
+      if (value.includes("-0")) {
+        value = value.replace("-", "");
+      } 
       return value;
     }
   }
@@ -768,7 +906,7 @@ function handleResponse(status, res, userID) {
       location.reload(); // Hier die Seite neu laden
     } else if(status === 400){
       res.redirect('/products');
-    } else if(status === 123){
+    } else if(status === 123 ||status === 500){
       getNoticitcation = 'Es wurde eine falsche Datei eingefügt'
       res.redirect('/products');
     } else if(status === 100){
